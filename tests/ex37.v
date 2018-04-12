5d90c1bcabf13086432d0a652c7298800df1d2be
mrmr1993 <mr_1993@hotmail.co.uk>
2018-04-12 21:29:15 +0100

    Add test 37 for module includes

---
+++ tests/ex37.v
@@ -0,0 +1,22 @@
+Require Import OCaml.OCaml.
+
+Local Open Scope Z_scope.
+Local Open Scope type_scope.
+Import ListNotations.
+
+Module A.
+  Definition a : Z := 5.
+  
+  Definition c {A : Type} (x : string) : M [ OCaml.Failure ] A :=
+    OCaml.Pervasives.failwith x.
+End A.
+
+Include A.
+
+Definition b : Z := Z.add a 2.
+
+Definition d {A : Type} (b : bool) : M [ OCaml.Failure ] A :=
+  if b then
+    c "true" % string
+  else
+    c "false" % string.
