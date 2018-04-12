c2c48b9e42c29c8b929d2665edad6a2318b3e0ce
mrmr1993 <mr_1993@hotmail.co.uk>
2018-04-12 15:33:48 +0100

    Move Envi.Mod to Envi

--- src/envi.ml
+++ src/envi.ml
@@ -1,126 +1,124 @@
 open SmartPrint
 
-module Mod = struct
-  type 'a t = {
-    opens : Name.t list list;
-    vars : 'a PathName.Map.t;
-    typs : unit PathName.Map.t;
-    descriptors: unit PathName.Map.t;
-    constructors : unit PathName.Map.t;
-    fields : unit PathName.Map.t }
+type 'a t = {
+  opens : Name.t list list;
+  vars : 'a PathName.Map.t;
+  typs : unit PathName.Map.t;
+  descriptors: unit PathName.Map.t;
+  constructors : unit PathName.Map.t;
+  fields : unit PathName.Map.t }
 
-  let empty : 'a t = {
-    opens = [[]]; (** By default we open the empty path. *)
-    vars = PathName.Map.empty;
-    typs = PathName.Map.empty;
-    descriptors = PathName.Map.empty;
-    constructors = PathName.Map.empty;
-    fields = PathName.Map.empty }
+let empty : 'a t = {
+  opens = [[]]; (** By default we open the empty path. *)
+  vars = PathName.Map.empty;
+  typs = PathName.Map.empty;
+  descriptors = PathName.Map.empty;
+  constructors = PathName.Map.empty;
+  fields = PathName.Map.empty }
 
-  let pp (m : 'a t) : SmartPrint.t =
-    let pp_map map = OCaml.list (fun (x, _) -> PathName.pp x)
-      (PathName.Map.bindings map) in
-    group (
-      nest (!^ "open" ^^ OCaml.list (fun path ->
-        double_quotes (separate (!^ ".") (List.map Name.pp path)))
-        m.opens) ^^ newline ^^
-      !^ "vars:" ^^ nest (pp_map m.vars) ^^ newline ^^
-      !^ "typs:" ^^ nest (pp_map m.typs) ^^ newline ^^
-      !^ "descriptors:" ^^ nest (pp_map m.descriptors) ^^ newline ^^
-      !^ "constructors:" ^^ nest (pp_map m.constructors) ^^ newline ^^
-      !^ "fields:" ^^ nest (pp_map m.fields))
+let pp (m : 'a t) : SmartPrint.t =
+  let pp_map map = OCaml.list (fun (x, _) -> PathName.pp x)
+    (PathName.Map.bindings map) in
+  group (
+    nest (!^ "open" ^^ OCaml.list (fun path ->
+      double_quotes (separate (!^ ".") (List.map Name.pp path)))
+      m.opens) ^^ newline ^^
+    !^ "vars:" ^^ nest (pp_map m.vars) ^^ newline ^^
+    !^ "typs:" ^^ nest (pp_map m.typs) ^^ newline ^^
+    !^ "descriptors:" ^^ nest (pp_map m.descriptors) ^^ newline ^^
+    !^ "constructors:" ^^ nest (pp_map m.constructors) ^^ newline ^^
+    !^ "fields:" ^^ nest (pp_map m.fields))
 
-  let open_module (m : 'a t) (module_name : Name.t list) : 'a t =
-    { m with opens = module_name :: m.opens }
+let open_module (m : 'a t) (module_name : Name.t list) : 'a t =
+  { m with opens = module_name :: m.opens }
 
-  let map_union (f : PathName.t -> PathName.t) (g : PathName.t -> 'a -> 'a)
-    (m1 : 'a PathName.Map.t) (m2 : 'a PathName.Map.t) : 'a PathName.Map.t =
-    PathName.Map.fold (fun name value newmap ->
-      PathName.Map.add (f name) (g name value) newmap
-    ) m1 m2
+let map_union (f : PathName.t -> PathName.t) (g : PathName.t -> 'a -> 'a)
+  (m1 : 'a PathName.Map.t) (m2 : 'a PathName.Map.t) : 'a PathName.Map.t =
+  PathName.Map.fold (fun name value newmap ->
+    PathName.Map.add (f name) (g name value) newmap
+  ) m1 m2
 
-  let close_module (module_name : Name.t) (prefix : Name.t -> 'a -> 'a)
-    (m1 : 'a t) (m2 : 'a t) : 'a t =
-    let add_to_path x =
-      { x with PathName.path = module_name :: x.PathName.path } in
-    let unit_map_union m1 m2 = map_union add_to_path (fun _ () -> ()) m1 m2 in
-  { opens = m2.opens;
-    vars = map_union add_to_path (fun _ v -> prefix module_name v)
-      m1.vars m2.vars;
-    typs = unit_map_union m1.typs m2.typs;
-    descriptors = unit_map_union m1.descriptors m2.descriptors;
-    constructors = unit_map_union m1.constructors m2.constructors;
-    fields = unit_map_union m1.fields m2.fields}
+let close_module (module_name : Name.t) (prefix : Name.t -> 'a -> 'a)
+  (m1 : 'a t) (m2 : 'a t) : 'a t =
+  let add_to_path x =
+    { x with PathName.path = module_name :: x.PathName.path } in
+  let unit_map_union m1 m2 = map_union add_to_path (fun _ () -> ()) m1 m2 in
+{ opens = m2.opens;
+  vars = map_union add_to_path (fun _ v -> prefix module_name v)
+    m1.vars m2.vars;
+  typs = unit_map_union m1.typs m2.typs;
+  descriptors = unit_map_union m1.descriptors m2.descriptors;
+  constructors = unit_map_union m1.constructors m2.constructors;
+  fields = unit_map_union m1.fields m2.fields}
 
-  let find_free_name (base_name : string) (env : 'a PathName.Map.t) : Name.t =
-    let prefix_n s n =
-      if n = 0 then
-        Name.of_string s
-      else
-        Name.of_string @@ Printf.sprintf "%s_%d" s n in
-    let rec first_n (n : int) : int =
-      if PathName.Map.mem (PathName.of_name [] @@ prefix_n base_name n) env then
-        first_n (n + 1)
-      else
-        n in
-    prefix_n base_name (first_n 0)
+let find_free_name (base_name : string) (env : 'a PathName.Map.t) : Name.t =
+  let prefix_n s n =
+    if n = 0 then
+      Name.of_string s
+    else
+      Name.of_string @@ Printf.sprintf "%s_%d" s n in
+  let rec first_n (n : int) : int =
+    if PathName.Map.mem (PathName.of_name [] @@ prefix_n base_name n) env then
+      first_n (n + 1)
+    else
+      n in
+  prefix_n base_name (first_n 0)
 
-  module Vars = struct
-    let add (x : PathName.t) (v : 'a) (m : 'a t) : 'a t =
-      { m with vars = PathName.Map.add x v m.vars }
+module Vars = struct
+  let add (x : PathName.t) (v : 'a) (m : 'a t) : 'a t =
+    { m with vars = PathName.Map.add x v m.vars }
 
-    let mem (x : PathName.t) (m : 'a t) : bool =
-      PathName.Map.mem x m.vars
+  let mem (x : PathName.t) (m : 'a t) : bool =
+    PathName.Map.mem x m.vars
 
-    let find (x : PathName.t) (m : 'a t) : 'a =
-      PathName.Map.find x m.vars
+  let find (x : PathName.t) (m : 'a t) : 'a =
+    PathName.Map.find x m.vars
 
-    let map (f : 'a -> 'b) (m : 'a t) : 'b t =
-      { m with vars = PathName.Map.map f m.vars }
+  let map (f : 'a -> 'b) (m : 'a t) : 'b t =
+    { m with vars = PathName.Map.map f m.vars }
 
-    (** Add a fresh local name beginning with [prefix] in [env]. *)
-    let fresh (prefix : string) (v : 'a) (env : 'a t) : Name.t * 'a t =
-      let name = find_free_name prefix env.vars in
-      (name, add (PathName.of_name [] name) v env)
-  end
-  module Typs = struct
-    let add (x : PathName.t) (m : 'a t) : 'a t =
-      { m with typs = PathName.Map.add x () m.typs }
+  (** Add a fresh local name beginning with [prefix] in [env]. *)
+  let fresh (prefix : string) (v : 'a) (env : 'a t) : Name.t * 'a t =
+    let name = find_free_name prefix env.vars in
+    (name, add (PathName.of_name [] name) v env)
+end
+module Typs = struct
+  let add (x : PathName.t) (m : 'a t) : 'a t =
+    { m with typs = PathName.Map.add x () m.typs }
 
-    let mem (x : PathName.t) (m : 'a t) : bool =
-      PathName.Map.mem x m.typs
+  let mem (x : PathName.t) (m : 'a t) : bool =
+    PathName.Map.mem x m.typs
 
-    let map (f : unit -> 'b) (m : 'a t) : 'b t =
-      { m with typs = PathName.Map.map f m.typs }
-  end
-  module Descriptors = struct
-    let add (x : PathName.t) (m : 'a t) : 'a t =
-      { m with descriptors = PathName.Map.add x () m.descriptors }
+  let map (f : unit -> 'b) (m : 'a t) : 'b t =
+    { m with typs = PathName.Map.map f m.typs }
+end
+module Descriptors = struct
+  let add (x : PathName.t) (m : 'a t) : 'a t =
+    { m with descriptors = PathName.Map.add x () m.descriptors }
 
-    let mem (x : PathName.t) (m : 'a t) : bool =
-      PathName.Map.mem x m.descriptors
+  let mem (x : PathName.t) (m : 'a t) : bool =
+    PathName.Map.mem x m.descriptors
 
-    let map (f : unit -> 'b) (m : 'a t) : 'b t =
-      { m with descriptors = PathName.Map.map f m.descriptors }
-  end
-  module Constructors = struct
-    let add (x : PathName.t) (m : 'a t) : 'a t =
-      { m with constructors = PathName.Map.add x () m.constructors }
+  let map (f : unit -> 'b) (m : 'a t) : 'b t =
+    { m with descriptors = PathName.Map.map f m.descriptors }
+end
+module Constructors = struct
+  let add (x : PathName.t) (m : 'a t) : 'a t =
+    { m with constructors = PathName.Map.add x () m.constructors }
 
-    let mem (x : PathName.t) (m : 'a t) : bool =
-      PathName.Map.mem x m.constructors
+  let mem (x : PathName.t) (m : 'a t) : bool =
+    PathName.Map.mem x m.constructors
 
-    let map (f : unit -> 'b) (m : 'a t) : 'b t =
-      { m with constructors = PathName.Map.map f m.constructors }
-  end
-  module Fields = struct
-    let add (x : PathName.t) (m : 'a t) : 'a t =
-      { m with fields = PathName.Map.add x () m.fields }
+  let map (f : unit -> 'b) (m : 'a t) : 'b t =
+    { m with constructors = PathName.Map.map f m.constructors }
+end
+module Fields = struct
+  let add (x : PathName.t) (m : 'a t) : 'a t =
+    { m with fields = PathName.Map.add x () m.fields }
 
-    let mem (x : PathName.t) (m : 'a t) : bool =
-      PathName.Map.mem x m.fields
+  let mem (x : PathName.t) (m : 'a t) : bool =
+    PathName.Map.mem x m.fields
 
-    let map (f : unit -> 'b) (m : 'a t) : 'b t =
-      { m with fields = PathName.Map.map f m.fields }
-  end
+  let map (f : unit -> 'b) (m : 'a t) : 'b t =
+    { m with fields = PathName.Map.map f m.fields }
 end
--- src/fullEnvi.ml
+++ src/fullEnvi.ml
@@ -1,55 +1,55 @@
 open SmartPrint
 
 module ModList = struct
-  type 'a t = 'a Envi.Mod.t list
+  type 'a t = 'a Envi.t list
 
   let pp (env : 'a t) : SmartPrint.t =
-    OCaml.list Envi.Mod.pp env
+    OCaml.list Envi.pp env
 
-  let empty : 'a t = [Envi.Mod.empty]
+  let empty : 'a t = [Envi.empty]
 
   let add_var (path : Name.t list) (base : Name.t) (v : 'a) (env : 'a t)
     : 'a t =
     match env with
-    | m :: env -> Envi.Mod.Vars.add (PathName.of_name path base) v m :: env
+    | m :: env -> Envi.Vars.add (PathName.of_name path base) v m :: env
     | [] -> failwith "The environment must be a non-empty list."
 
   let add_typ (path : Name.t list) (base : Name.t) (env : 'a t)
     : 'a t =
     match env with
-    | m :: env -> Envi.Mod.Typs.add (PathName.of_name path base) m :: env
+    | m :: env -> Envi.Typs.add (PathName.of_name path base) m :: env
     | [] -> failwith "The environment must be a non-empty list."
 
   let add_descriptor (path : Name.t list) (base : Name.t) (env : 'a t)
     : 'a t =
     match env with
-    | m :: env -> Envi.Mod.Descriptors.add (PathName.of_name path base) m :: env
+    | m :: env -> Envi.Descriptors.add (PathName.of_name path base) m :: env
     | [] -> failwith "The environment must be a non-empty list."
 
   let add_constructor (path : Name.t list) (base : Name.t) (env : 'a t)
     : 'a t =
     match env with
-    | m :: env -> Envi.Mod.Constructors.add (PathName.of_name path base) m :: env
+    | m :: env -> Envi.Constructors.add (PathName.of_name path base) m :: env
     | [] -> failwith "The environment must be a non-empty list."
 
   let add_field (path : Name.t list) (base : Name.t) (env : 'a t)
     : 'a t =
     match env with
-    | m :: env -> Envi.Mod.Fields.add (PathName.of_name path base) m :: env
+    | m :: env -> Envi.Fields.add (PathName.of_name path base) m :: env
     | [] -> failwith "The environment must be a non-empty list."
 
-  let enter_module (env : 'a t) : 'a t = Envi.Mod.empty :: env
+  let enter_module (env : 'a t) : 'a t = Envi.empty :: env
 
   let open_module (module_name : Name.t list) (env : 'a t) : 'a t =
     match env with
-    | m :: env -> Envi.Mod.open_module m module_name :: env
+    | m :: env -> Envi.open_module m module_name :: env
     | _ -> failwith "You should have entered in at least one module."
 
   let leave_module (module_name : Name.t) (prefix : Name.t -> 'a -> 'a)
     (env : 'a t) : 'a t =
     match env with
     | m1 :: m2 :: env ->
-      let m = Envi.Mod.close_module module_name prefix m1 m2 in
+      let m = Envi.close_module module_name prefix m1 m2 in
       m :: env
     | _ -> failwith "You should have entered in at least one module."
 
@@ -61,21 +61,21 @@ module ModList = struct
       | None -> find_first f l
       | y -> y)
 
-  let rec bound_name_opt (find : PathName.t -> 'a Envi.Mod.t -> bool)
+  let rec bound_name_opt (find : PathName.t -> 'a Envi.t -> bool)
     (x : PathName.t) (env : 'a t) : BoundName.t option =
     match env with
     | m :: env ->
       if find x m then
         Some { BoundName.path_name = x; BoundName.depth = 0 }
       else
-        m.Envi.Mod.opens |> find_first (fun path ->
+        m.Envi.opens |> find_first (fun path ->
           let x = { x with PathName.path = path @ x.PathName.path } in
           match bound_name_opt find x env with
           | None -> None
           | Some name -> Some { name with BoundName.depth = name.BoundName.depth + 1 })
     | [] -> None
 
-  let bound_name (find : PathName.t -> 'a Envi.Mod.t -> bool) (loc : Loc.t)
+  let bound_name (find : PathName.t -> 'a Envi.t -> bool) (loc : Loc.t)
     (x : PathName.t) (env : 'a t) : BoundName.t =
     match bound_name_opt find x env with
     | Some name -> name
@@ -84,19 +84,19 @@ module ModList = struct
       Error.raise loc (SmartPrint.to_string 80 2 message)
 
   let bound_var (loc : Loc.t) (x : PathName.t) (env : 'a t) : BoundName.t =
-    bound_name Envi.Mod.Vars.mem loc x env
+    bound_name Envi.Vars.mem loc x env
 
   let bound_typ (loc : Loc.t) (x : PathName.t) (env : 'a t) : BoundName.t =
-    bound_name Envi.Mod.Typs.mem loc x env
+    bound_name Envi.Typs.mem loc x env
 
   let bound_descriptor (loc : Loc.t) (x : PathName.t) (env : 'a t) : BoundName.t =
-    bound_name Envi.Mod.Descriptors.mem loc x env
+    bound_name Envi.Descriptors.mem loc x env
 
   let bound_constructor (loc : Loc.t) (x : PathName.t) (env : 'a t) : BoundName.t =
-    bound_name Envi.Mod.Constructors.mem loc x env
+    bound_name Envi.Constructors.mem loc x env
 
   let bound_field (loc : Loc.t) (x : PathName.t) (env : 'a t) : BoundName.t =
-    bound_name Envi.Mod.Fields.mem loc x env
+    bound_name Envi.Fields.mem loc x env
 
   let add_exception (path : Name.t list) (base : Name.t) (env : unit t) : unit t =
     env
@@ -124,18 +124,18 @@ module ModList = struct
         v
       else
         iterate_open_lift (open_lift v) (n - 1) in
-    let v = Envi.Mod.Vars.find x.BoundName.path_name m in
+    let v = Envi.Vars.find x.BoundName.path_name m in
     iterate_open_lift v x.BoundName.depth
 
   let fresh_var  (prefix : string) (v : 'a) (env : 'a t) : Name.t * 'a t =
     match env with
     | m :: env ->
-      let (name, m) = Envi.Mod.Vars.fresh prefix v m in
+      let (name, m) = Envi.Vars.fresh prefix v m in
       (name, m :: env)
     | [] -> failwith "The environment must be a non-empty list."
 
   let rec map (f : 'a -> 'b) (env : 'a t) : 'b t =
     match env with
-    | m :: env -> Envi.Mod.Vars.map f m :: map f env
+    | m :: env -> Envi.Vars.map f m :: map f env
     | [] -> []
 end
