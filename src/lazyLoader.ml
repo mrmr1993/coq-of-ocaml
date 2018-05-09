834363e3cddc87fb750f89b5c81d9606cb6b09fb
mrmr1993 <mr_1993@hotmail.co.uk>
2018-05-09 22:10:10 +0100

    Use -I command line argument to specify interface directories

--- src/lazyLoader.ml
+++ src/lazyLoader.ml
@@ -18,15 +18,23 @@ let add_interface (env : Effect.Type.t FullEnvi.t) (coq_prefix : Name.t)
   let wmod = Interface.to_wrapped_mod coq_prefix interface env in
   (wmod, add_wrapped_mod wmod loader)
 
+let find_first (f : 'a -> 'b option) (l : 'a list) : 'b option =
+  FullMod.find_first f l
+
+(* Mutable list of Coq name, interface directory pairs. *)
+let interfaces : (Name.t * string) list ref = ref []
+
 let find_wrapped_mod_opt (env : Effect.Type.t FullEnvi.t) (loader : 'a t)
   (module_name : Name.t) : 'a FullEnvi.WrappedMod.t option * 'a t =
   match Name.Map.find_opt module_name loader with
   | Some wmod -> (Some wmod, loader)
   | None ->
     let file_name = String.uncapitalize_ascii (Name.to_string module_name) in
-    let file_name = "interfaces/" ^ file_name ^ ".interface" in
-    if Sys.file_exists file_name then
-      let (wmod, loader) = add_interface env "OCaml" loader file_name in
+    match find_first (fun (coq_prefix, dir) ->
+        let file_name = Filename.concat dir (file_name ^ ".interface") in
+        if Sys.file_exists file_name then Some (coq_prefix, file_name) else None)
+      !interfaces with
+    | Some (coq_prefix, file_name) ->
+      let (wmod, loader) = add_interface env coq_prefix loader file_name in
       (Some wmod, loader)
-    else
-      (None, loader)
+    | None -> (None, loader)
