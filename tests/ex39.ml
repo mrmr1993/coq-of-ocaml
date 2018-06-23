aa0133de9e87faddf853758d0e6479d13f68196c
mrmr1993 <mr_1993@hotmail.co.uk>
2018-06-23 19:38:06 +0100

    Add tests for arguments with type ref

--- tests/ex39.ml
+++ tests/ex39.ml
@@ -17,3 +17,14 @@ let add_multiple_by_refs a b c d : int =
   let z = ref (!x + !y) in
   !z
 
+let set_ref (x : int ref) : unit =
+  x := 15
+
+let get_ref (x : int ref) : int =
+  !x
+
+let update_ref (x : int ref) : unit =
+  x := !x + 5
+
+let new_ref (x : unit) : int ref =
+  ref 15
