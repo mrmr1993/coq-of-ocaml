9bc6db9e6a2ca7defa75e7908ae5dda5a3ef402b
mrmr1993 <mr_1993@hotmail.co.uk>
2018-06-24 14:33:17 +0100

    Update tests output for new reference implementation

--- tests/ex18.v
+++ tests/ex18.v
@@ -4,36 +4,66 @@ Local Open Scope Z_scope.
 Local Open Scope type_scope.
 Import ListNotations.
 
-Definition r := OCaml.Effect.State.init 12.
-Definition r_state := @OCaml.Effect.State.state Z.
+Definition r : OCaml.Effect.State.t Z := OCaml.Effect.State.init 12.
+Definition r_state := nat.
 
-Definition plus_one {A : Type} (x : A) : M [ r_state ] Z :=
-  match x with
-  | _ =>
-    let! x_1 := OCaml.Effect.State.read r in
-    ret (Z.add x_1 1)
-  end.
-
-Definition s := OCaml.Effect.State.init "Hi" % string.
-Definition s_state := @OCaml.Effect.State.state string.
-
-Definition fail {A B : Type} (x : A) : M [ s_state; OCaml.Failure ] B :=
-  match x with
-  | _ =>
-    let! x_1 := lift [_;_] "10" (OCaml.Effect.State.read s) in
-    lift [_;_] "01" (OCaml.Pervasives.failwith x_1)
-  end.
-
-Definition reset {A : Type} (x : A) : M [ r_state ] unit :=
-  match x with
-  | _ => OCaml.Effect.State.write r 0
-  end.
-
-Definition incr {A : Type} (x : A) : M [ r_state ] unit :=
+Definition plus_one {A : Type} (x : A)
+  : M [ OCaml.Effect.State.state Z; OCaml.Effect.State.state r_state ] Z :=
   match x with
   | _ =>
     let! x_1 :=
-      let! x_1 := OCaml.Effect.State.read r in
-      ret (Z.add x_1 1) in
-    OCaml.Effect.State.write r x_1
+      let! x_1 :=
+        let! x_1 := lift [_;_] "10" (OCaml.Effect.State.peekstate tt) in
+        lift [_;_] "01" (OCaml.Effect.State.global r x_1) in
+      lift [_;_] "10" (OCaml.Effect.State.read x_1) in
+    ret (Z.add x_1 1)
+  end.
+
+Definition s : OCaml.Effect.State.t string := OCaml.Effect.State.init
+  "Hi" % string.
+Definition s_state := nat.
+
+Definition fail {A B : Type} (x : A)
+  : M
+    [
+      OCaml.Failure;
+      OCaml.Effect.State.state s_state;
+      OCaml.Effect.State.state string
+    ] B :=
+  match x with
+  | _ =>
+    let! x_1 :=
+      lift [_;_;_] "011"
+        (let! x_1 :=
+          let! x_1 := lift [_;_] "01" (OCaml.Effect.State.peekstate tt) in
+          lift [_;_] "10" (OCaml.Effect.State.global s x_1) in
+        lift [_;_] "01" (OCaml.Effect.State.read x_1)) in
+    lift [_;_;_] "100" (OCaml.Pervasives.failwith x_1)
+  end.
+
+Definition reset {A : Type} (x : A)
+  : M [ OCaml.Effect.State.state Z; OCaml.Effect.State.state r_state ] unit :=
+  match x with
+  | _ =>
+    let! x_1 :=
+      let! x_1 := lift [_;_] "10" (OCaml.Effect.State.peekstate tt) in
+      lift [_;_] "01" (OCaml.Effect.State.global r x_1) in
+    lift [_;_] "10" (OCaml.Effect.State.write x_1 0)
+  end.
+
+Definition incr {A : Type} (x : A)
+  : M [ OCaml.Effect.State.state Z; OCaml.Effect.State.state r_state ] unit :=
+  match x with
+  | _ =>
+    let! x_1 :=
+      let! x_1 := lift [_;_] "10" (OCaml.Effect.State.peekstate tt) in
+      lift [_;_] "01" (OCaml.Effect.State.global r x_1) in
+    let! x_2 :=
+      let! x_2 :=
+        let! x_2 :=
+          let! x_2 := lift [_;_] "10" (OCaml.Effect.State.peekstate tt) in
+          lift [_;_] "01" (OCaml.Effect.State.global r x_2) in
+        lift [_;_] "10" (OCaml.Effect.State.read x_2) in
+      ret (Z.add x_2 1) in
+    lift [_;_] "10" (OCaml.Effect.State.write x_1 x_2)
   end.
--- tests/ex39.v
+++ tests/ex39.v
@@ -4,17 +4,17 @@ Local Open Scope Z_scope.
 Local Open Scope type_scope.
 Import ListNotations.
 
-Definition get_local_ref (tt : unit) : M [ OCaml.Effect.State.state ] Z :=
+Definition get_local_ref (tt : unit) : M [ OCaml.Effect.State.state Z ] Z :=
   let! x := OCaml.Pervasives.ref 12 in
   OCaml.Effect.State.read x.
 
-Definition set_local_ref (tt : unit) : M [ OCaml.Effect.State.state ] Z :=
+Definition set_local_ref (tt : unit) : M [ OCaml.Effect.State.state Z ] Z :=
   let! x := OCaml.Pervasives.ref 12 in
   let! _ := OCaml.Effect.State.write x 15 in
   OCaml.Effect.State.read x.
 
 Definition add_multiple_by_refs (a : Z) (b : Z) (c : Z) (d : Z)
-  : M [ OCaml.Effect.State.state ] Z :=
+  : M [ OCaml.Effect.State.state Z ] Z :=
   let! x := OCaml.Pervasives.ref a in
   let! _ :=
     let! x_1 :=
@@ -36,18 +36,50 @@ Definition add_multiple_by_refs (a : Z) (b : Z) (c : Z) (d : Z)
   OCaml.Effect.State.read z.
 
 Definition set_ref (x : OCaml.Effect.State.t Z)
-  : M [ OCaml.Effect.State.state ] unit := OCaml.Effect.State.write x 15.
+  : M [ OCaml.Effect.State.state Z ] unit := OCaml.Effect.State.write x 15.
 
 Definition get_ref (x : OCaml.Effect.State.t Z)
-  : M [ OCaml.Effect.State.state ] Z := OCaml.Effect.State.read x.
+  : M [ OCaml.Effect.State.state Z ] Z := OCaml.Effect.State.read x.
 
 Definition update_ref (x : OCaml.Effect.State.t Z)
-  : M [ OCaml.Effect.State.state ] unit :=
+  : M [ OCaml.Effect.State.state Z ] unit :=
   let! x_1 :=
     let! x_1 := OCaml.Effect.State.read x in
     ret (Z.add x_1 5) in
   OCaml.Effect.State.write x x_1.
 
 Definition new_ref (x : unit)
-  : M [ OCaml.Effect.State.state ] (OCaml.Effect.State.t Z) :=
+  : M [ OCaml.Effect.State.state Z ] (OCaml.Effect.State.t Z) :=
   OCaml.Pervasives.ref 15.
+
+Definition r : OCaml.Effect.State.t Z := OCaml.Effect.State.init 18.
+Definition r_state := nat.
+
+Definition set_r (x : unit)
+  : M [ OCaml.Effect.State.state Z; OCaml.Effect.State.state r_state ] unit :=
+  let! x_1 :=
+    let! x_1 := lift [_;_] "10" (OCaml.Effect.State.peekstate tt) in
+    lift [_;_] "01" (OCaml.Effect.State.global r x_1) in
+  lift [_;_] "10" (set_ref x_1).
+
+Definition get_r (x : unit)
+  : M [ OCaml.Effect.State.state Z; OCaml.Effect.State.state r_state ] Z :=
+  let! x_1 :=
+    let! x_1 := lift [_;_] "10" (OCaml.Effect.State.peekstate tt) in
+    lift [_;_] "01" (OCaml.Effect.State.global r x_1) in
+  lift [_;_] "10" (get_ref x_1).
+
+Definition r_add_15 (x : unit)
+  : M [ OCaml.Effect.State.state Z; OCaml.Effect.State.state r_state ] Z :=
+  let! i := get_r tt in
+  let! _ := set_r tt in
+  let! j := get_r tt in
+  let! _ :=
+    let! x_1 :=
+      let! x_1 := lift [_;_] "10" (OCaml.Effect.State.peekstate tt) in
+      lift [_;_] "01" (OCaml.Effect.State.global r x_1) in
+    lift [_;_] "10" (OCaml.Effect.State.write x_1 (Z.add i j)) in
+  let! x_1 :=
+    let! x_1 := lift [_;_] "10" (OCaml.Effect.State.peekstate tt) in
+    lift [_;_] "01" (OCaml.Effect.State.global r x_1) in
+  lift [_;_] "10" (OCaml.Effect.State.read x_1).
