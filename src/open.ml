7ede94f9ec882d8d504e2a86eac4056cfc6fdb25
mrmr1993 <mr_1993@hotmail.co.uk>
2018-07-28 19:20:32 +0100

    Remove redundant code in Open; was already done in resolving BoundName.t

--- src/open.ml
+++ src/open.ml
@@ -7,17 +7,7 @@ let pp (o : t) : SmartPrint.t =
 
 let of_ocaml (env : 'a FullEnvi.t) (loc : Loc.t) (path : Path.t) : t =
   let path = PathName.of_path loc path in
-  let bound_name = FullEnvi.bound_module loc path env in
-  if bound_name.depth == -1 then
-    let m = FullEnvi.find_module bound_name env (fun x -> x) in
-    let (_, coq_name) = CoqName.assoc_names @@ Mod.name m in
-    let path = match bound_name.path_name.path with
-      | [] -> (* Toplevel module *)
-        { PathName.path = []; base = coq_name }
-      | external_module :: path ->
-        { bound_name.path_name with path = coq_name :: path } in
-    { bound_name with path_name = path }
-  else bound_name
+  FullEnvi.bound_module loc path env
 
 let update_env (loc : Loc.t) (o : t) (env : 'a FullEnvi.t) : 'a FullEnvi.t =
   FullEnvi.open_module o env
