216b3c99748e4b81f4c1b909bc14fc2fab65a8d8
mrmr1993 <mr_1993@hotmail.co.uk>
2018-08-23 22:24:33 +0100

    Add tests for type resolution/unification, Update expected test output

--- tests/ex39.v
+++ tests/ex39.v
@@ -55,24 +55,21 @@ Definition new_ref (x : unit)
   OCaml.Pervasives.ref 15.
 
 Definition r : OCaml.Effect.State.t Z := OCaml.Effect.State.init 18.
-Definition r_state := nat.
+Definition r_state := OCaml.Effect.State.state nat.
 
-Definition set_r (x : unit)
-  : M [ OCaml.Effect.State.state Z; OCaml.Effect.State.state r_state ] unit :=
+Definition set_r (x : unit) : M [ OCaml.Effect.State.state Z; r_state ] unit :=
   let! x_1 :=
     let! x_1 := lift [_;_] "10" (OCaml.Effect.State.peekstate tt) in
     lift [_;_] "01" (OCaml.Effect.State.global r x_1) in
   lift [_;_] "10" (set_ref x_1).
 
-Definition get_r (x : unit)
-  : M [ OCaml.Effect.State.state Z; OCaml.Effect.State.state r_state ] Z :=
+Definition get_r (x : unit) : M [ OCaml.Effect.State.state Z; r_state ] Z :=
   let! x_1 :=
     let! x_1 := lift [_;_] "10" (OCaml.Effect.State.peekstate tt) in
     lift [_;_] "01" (OCaml.Effect.State.global r x_1) in
   lift [_;_] "10" (get_ref x_1).
 
-Definition r_add_15 (x : unit)
-  : M [ OCaml.Effect.State.state Z; OCaml.Effect.State.state r_state ] Z :=
+Definition r_add_15 (x : unit) : M [ OCaml.Effect.State.state Z; r_state ] Z :=
   let! i := get_r tt in
   let! _ := set_r tt in
   let! j := get_r tt in
@@ -86,49 +83,71 @@ Definition r_add_15 (x : unit)
     lift [_;_] "01" (OCaml.Effect.State.global r x_1) in
   lift [_;_] "10" (OCaml.Effect.State.read x_1).
 
-Definition mixed_type (x : unit)
+Definition mixed_type {es_in : list Effect.t} (x : unit)
   : M
     [
-      OCaml.Effect.State.state Z;
-      OCaml.Effect.State.state bool;
-      OCaml.Effect.State.state r_state;
-      OCaml.Effect.State.state string
+      OCaml.Effect.Union.union es_in
+        [
+          (OCaml.Effect.State.state Z);
+          (OCaml.Effect.State.state bool);
+          (OCaml.Effect.State.state string)
+        ];
+      r_state
     ] (bool * string * Z) :=
-  let! b := lift [_;_;_;_] "0100" (OCaml.Pervasives.ref true) in
-  let! str := lift [_;_;_;_] "0001" (OCaml.Pervasives.ref "" % string) in
+  let! b :=
+    lift [_;_] "10" (@Union.lift _ _ _ [_;_;_] 1 (OCaml.Pervasives.ref true)) in
+  let! str :=
+    lift [_;_] "10"
+      (@Union.lift _ _ _ [_;_;_] 2 (OCaml.Pervasives.ref "" % string)) in
   let update (x_1 : unit)
-    : M [ OCaml.Effect.State.state bool; OCaml.Effect.State.state string ] unit :=
+    : M
+      [
+        OCaml.Effect.Union.union _
+          [
+            (OCaml.Effect.State.state bool);
+            (OCaml.Effect.State.state string)
+          ]
+      ] unit :=
     match x_1 with
     | tt =>
       let! _ :=
-        lift [_;_] "10"
+        @Union.lift _ _ _ [_;_] 0
           (let! x_2 := OCaml.Effect.State.read b in
           OCaml.Effect.State.write b x_2) in
-      lift [_;_] "01"
+      @Union.lift _ _ _ [_;_] 1
         (let! x_2 :=
           let! x_2 := OCaml.Effect.State.read str in
           ret (String.append "toggle " % string x_2) in
         OCaml.Effect.State.write str x_2)
     end in
-  let! _ := lift [_;_;_;_] "0101" (update tt) in
-  let! _ := lift [_;_;_;_] "0101" (update tt) in
-  let! _ := lift [_;_;_;_] "0101" (update tt) in
-  let! x_1 := lift [_;_;_;_] "0100" (OCaml.Effect.State.read b) in
-  let! x_2 := lift [_;_;_;_] "0001" (OCaml.Effect.State.read str) in
+  let! _ :=
+    lift [_;_] "10"
+      (@Union.mix _ _ _ _ [_;_;_] [1;2]%nat eq_refl eq_refl (update tt)) in
+  let! _ :=
+    lift [_;_] "10"
+      (@Union.mix _ _ _ _ [_;_;_] [1;2]%nat eq_refl eq_refl (update tt)) in
+  let! _ :=
+    lift [_;_] "10"
+      (@Union.mix _ _ _ _ [_;_;_] [1;2]%nat eq_refl eq_refl (update tt)) in
+  let! x_1 :=
+    lift [_;_] "10" (@Union.lift _ _ _ [_;_;_] 1 (OCaml.Effect.State.read b)) in
+  let! x_2 :=
+    lift [_;_] "10" (@Union.lift _ _ _ [_;_;_] 2 (OCaml.Effect.State.read str))
+    in
   let! x_3 :=
-    lift [_;_;_;_] "1010"
+    @Union.lift _ _ _ [_;_;_] 0
       (let! x_3 :=
         let! x_3 := lift [_;_] "10" (OCaml.Effect.State.peekstate tt) in
         lift [_;_] "01" (OCaml.Effect.State.global r x_3) in
       lift [_;_] "10" (OCaml.Effect.State.read x_3)) in
   ret (x_1, x_2, x_3).
 
-Definition partials_test (x : unit)
+Definition partials_test {es_in : list Effect.t} (x : unit)
   : M
     [
-      OCaml.Effect.State.state Z;
-      OCaml.Effect.State.state (list Z);
-      OCaml.Effect.State.state r_state
+      OCaml.Effect.Union.union es_in
+        [ (OCaml.Effect.State.state Z); (OCaml.Effect.State.state (list Z)) ];
+      r_state
     ] (OCaml.Effect.State.t Z) :=
   match x with
   | tt =>
@@ -137,29 +156,35 @@ Definition partials_test (x : unit)
       let! _ := OCaml.Effect.State.write x y in
       ret x in
     let! f1_test :=
-      lift [_;_;_] "101"
+      @Union.lift _ _ _ [_;_] 0
         (let! x_1 :=
           let! x_1 := lift [_;_] "10" (OCaml.Effect.State.peekstate tt) in
           lift [_;_] "01" (OCaml.Effect.State.global r x_1) in
         ret (f1 x_1)) in
-    lift [_;_;_] "110"
-      (let! f1_test := lift [_;_] "10" (f1_test 15) in
+    lift [_;_] "10"
+      (let! f1_test := @Union.lift _ _ _ [_;_] 0 (f1_test 15) in
       let f2 (l1 : OCaml.Effect.State.t (list Z)) (l2 : list string)
-        : M [ OCaml.Effect.State.state Z; OCaml.Effect.State.state (list Z) ]
-          (OCaml.Effect.State.t Z) :=
+        : M
+          [
+            OCaml.Effect.Union.union _
+              [
+                (OCaml.Effect.State.state Z);
+                (OCaml.Effect.State.state (list Z))
+              ]
+          ] (OCaml.Effect.State.t Z) :=
         let! x_1 :=
-          lift [_;_] "01"
+          @Union.lift _ _ _ [_;_] 1
             (let! x_1 :=
               let! x_1 := OCaml.Effect.State.read l1 in
               ret (OCaml.List.length x_1) in
             ret (Z.add x_1 (OCaml.List.length l2))) in
-        lift [_;_] "10" (OCaml.Pervasives.ref x_1) in
+        @Union.lift _ _ _ [_;_] 0 (OCaml.Pervasives.ref x_1) in
       let! f2_test :=
-        lift [_;_] "01"
+        @Union.lift _ _ _ [_;_] 1
           (let! x_1 := OCaml.Pervasives.ref (cons 1 (cons 2 (cons 3 []))) in
           ret (f2 x_1)) in
       let! f2_test := f2_test (cons "hi" % string (cons "hey" % string [])) in
-      lift [_;_] "10"
+      @Union.lift _ _ _ [_;_] 0
         (let! x_1 := OCaml.Effect.State.read f1_test in
         f1 f2_test x_1))
   end.
@@ -202,3 +227,32 @@ Definition multiple_returns_test (x : unit)
     let! x_1 := OCaml.Effect.State.read f4 in
     ret (x_1, s)
   end.
+
+Definition type_vars_test {A B : Type} {es_in : list Effect.t}
+  (x : OCaml.Effect.State.t A) (y : OCaml.Effect.State.t B) (a : A) (b : B)
+  : M
+    [
+      OCaml.Effect.Union.union es_in
+        [ (OCaml.Effect.State.state A); (OCaml.Effect.State.state B) ]
+    ] unit :=
+  let! _ := @Union.lift _ _ _ [_;_] 0 (OCaml.Effect.State.write x a) in
+  @Union.lift _ _ _ [_;_] 1 (OCaml.Effect.State.write y b).
+
+Definition resolves_test1 {A : Type}
+  (x : OCaml.Effect.State.t A) (a : A) (b : A)
+  : M [ OCaml.Effect.State.state A ] unit :=
+  Union.inject 2 (type_vars_test x x a b).
+
+Definition resolves_test2 {A : Type} {es_in : list Effect.t}
+  (x : OCaml.Effect.State.t Z) (y : OCaml.Effect.State.t A) (a : Z) (b : A)
+  : M
+    [
+      OCaml.Effect.Union.union es_in
+        [ (OCaml.Effect.State.state A); (OCaml.Effect.State.state Z) ]
+    ] unit :=
+  @Union.mix _ _ _ _ [_;_] [1;0]%nat eq_refl eq_refl (type_vars_test x y a b).
+
+Definition resolves_test3
+  (x : OCaml.Effect.State.t Z) (y : OCaml.Effect.State.t Z) (a : Z) (b : Z)
+  : M [ OCaml.Effect.State.state Z ] unit :=
+  Union.inject 2 (type_vars_test x y a b).
