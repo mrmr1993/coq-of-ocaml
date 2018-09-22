b52742bae916c85e8d749f8f39c4f5a87a98ec23
mrmr1993 <mr_1993@hotmail.co.uk>
2018-09-22 17:32:23 +0100

    Add more tests, update test output for destructuring toplevel lets

--- tests/ex44.ml
+++ tests/ex44.ml
@@ -7,3 +7,14 @@ let (a, b) = (15, "hi")
 type a = A of int * bool
 
 let A (i, j) = A (15, true)
+
+type b = { first : int; second : bool; third : string }
+
+let b_val = { first = 12; second = false; third = "hello" }
+
+let { first = hi } = b_val
+let { second = hey } = b_val
+let { second = hey; third = hello } = b_val
+
+let { first = first; second = second; third = third } =
+  { first = hi; second = hey; third = hello }
--- tests/ex44.v
+++ tests/ex44.v
@@ -8,12 +8,12 @@ Definition temp : unit := OCaml.Pervasives.ignore (Z.add 1 1).
 
 Definition temp_1 : Z * string := (15, "hi" % string).
 
-Definition a : _ :=
+Definition a : Z :=
   match temp_1 with
   | (a, b) => a
   end.
 
-Definition b : _ :=
+Definition b : string :=
   match temp_1 with
   | (a, b) => b
   end.
@@ -23,12 +23,59 @@ Inductive a_1 : Type :=
 
 Definition temp_2 : a_1 := A 15 true.
 
-Definition i : _ :=
+Definition i : Z :=
   match temp_2 with
   | A i j => i
   end.
 
-Definition j : _ :=
+Definition j : bool :=
   match temp_2 with
   | A i j => j
+  end.
+
+Record b_1 : Type := {
+  first : Z;
+  second : bool;
+  third : string }.
+
+Definition b_val : b_1 :=
+  {| first := 12; second := false; third := "hello" % string |}.
+
+Definition hi : Z :=
+  match b_val with
+  | {| first := hi |} => hi
+  end.
+
+Definition hey : bool :=
+  match b_val with
+  | {| second := hey |} => hey
+  end.
+
+Definition temp_5 : b_1 := b_val.
+
+Definition hello : string :=
+  match temp_5 with
+  | {| second := hey_1; third := hello |} => hello
+  end.
+
+Definition hey_1 : bool :=
+  match temp_5 with
+  | {| second := hey_1; third := hello |} => hey_1
+  end.
+
+Definition temp_6 : b_1 := {| first := hi; second := hey_1; third := hello |}.
+
+Definition first_1 : Z :=
+  match temp_6 with
+  | {| first := first_1; second := second_1; third := third_1 |} => first_1
+  end.
+
+Definition second_1 : bool :=
+  match temp_6 with
+  | {| first := first_1; second := second_1; third := third_1 |} => second_1
+  end.
+
+Definition third_1 : string :=
+  match temp_6 with
+  | {| first := first_1; second := second_1; third := third_1 |} => third_1
   end.
